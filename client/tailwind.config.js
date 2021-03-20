5592a2d08d79e64a0c0d5685d49dffec2f00e04a
SugarcubeIsCoding <Kimberly.vandenbroeck@hotmail.com>
2021-03-20 15:58:24 +0100

    colors, fonts, screen breakpoints

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,10 +2,32 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    screens: {
+      xs: "480px",
+      sm: "660px",
+      md: "820px",
+      lg: "980px",
+      xl: "1200px",
+      fontFamily: {
+        heading: ['Open Sans', ...defaultTheme.fontFamily.sans],
+        primary: ['Poppins', ...defaultTheme.fontFamily.sans],
+        quote: ['Fondamento', ...defaultTheme.fontFamily.cursive],
+      },
+    extend: {
+      color: {
+        'primary': '#06BDE1', 
+        'black': '#333333',
+        'white': '#FFFFFF'
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
+  }
 }
