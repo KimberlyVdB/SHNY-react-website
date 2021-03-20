8913047589f1850cf7ef6aea38ff9961efb8ac5c
Muhammed Bacalan <muhammedbacalan@gmail.com>
2021-03-20 18:17:49 +0300

    Create components folder and example header

---
+++ client/src/components/header/index.js
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>SHNY</h1>
+
+        <ul>
+          <li>Home</li>
+          <li>News</li>
+          <li>Apply</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
