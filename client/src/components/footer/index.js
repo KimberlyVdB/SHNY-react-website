373858798106def32edf14ce86c0c3b7ba07d2de
SugarcubeIsCoding <Kimberly.vandenbroeck@hotmail.com>
2021-03-27 12:02:08 +0100

    homepage, nav, footer and needed assets

---
+++ client/src/components/footer/index.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import './index.css';
+
+
+function Footer() {
+  return (
+    <footer class="footer">
+      <p>Made by <a href="https://www.instagram.com/sugarcube.gw2/">Sugarcube</a> powerd by Penemue</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,6 +8,15 @@ module.exports = {
       center: true,
       padding: '1rem',
     },
+    borderWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
+    },
     screens: {
       xs: "480px",
       sm: "660px",
