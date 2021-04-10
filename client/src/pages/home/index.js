8fb69a224cee005e6b308cf19d46b263b5f2a6e8
SugarcubeIsCoding <Kimberly.vandenbroeck@hotmail.com>
2021-04-10 23:31:40 +0200

    homepage changes

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,12 +1,16 @@
 import './index.css';
+// import heart from '../../icons/heart.svg';
+
 
 function Home() {
   return (
     <section className="homepage-bg">
       <div className="homepage">
-        <h1 className="homepage-title trail-line-double">A Skritt At Heart</h1>
-        <h3>We want you in the Skritt army!</h3>
-        <a href="/" className="cta-btn">Enlist Today</a>
+        <h1 className="uppercase homepage-title trail-line-double">A Skritt At Heart </h1>
+        <h3>We want you in the Skritt army today! </h3>
+        <div class="cta-div">
+        <a href="/" className="cta-btn">Enlist Today </a>
+        </div>
       </div>
     </section>
   );
