c983b9238a0c0fd851030453b240ec4f325ea640
SugarcubeIsCoding <Kimberly.vandenbroeck@hotmail.com>
2021-04-10 23:32:22 +0200

    footer changes

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -1,9 +1,10 @@
 import './index.css';
+// import heart from '../../icons/heart.svg';
 
 function Footer() {
   return (
     <footer class="footer">
-      <p>Made by <a href="https://www.instagram.com/sugarcube.gw2/">Sugarcube</a> powered by Penemue</p>
+      <p>Made by <a href="https://www.instagram.com/sugarcube.gw2/">Sugarcube </a> powered by Penemue</p>
     </footer>
   );
 }
