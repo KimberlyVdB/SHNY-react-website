5939574d0dcae94f508d1040ff94bb2311a6cba9
SugarcubeIsCoding <Kimberly.vandenbroeck@hotmail.com>
2021-04-24 17:16:39 +0200

    changes to home + socials svgs

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,15 +1,20 @@
 import './index.css';
-// import heart from '../../icons/heart.svg';
-
+import youtube from '../../icons/youtube.svg';
+import instagram from '../../icons/instagram.svg';
 
 function Home() {
   return (
-    <section className="homepage-bg">
+    <section className="">
       <div className="homepage">
         <h1 className="uppercase homepage-title trail-line-double">A Skritt At Heart </h1>
         <h3>We want you in the Skritt army today! </h3>
         <div class="cta-div">
         <a href="/" className="cta-btn">Enlist Today </a>
+        </div>
+        <div class="social-media">
+        <a href="https://www.instagram.com/a_skritt_at_heart/"><img alt="instagram" class="social-ig" src={instagram}></img></a>
+        <a href="/"><img alt="youtube" class="social-yt"  src={youtube}></img></a>
+
         </div>
       </div>
     </section>
