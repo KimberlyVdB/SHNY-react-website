7de2b11ee5d8b9182e8e2cfb33e7be68bddbe314
SugarcubeIsCoding <Kimberly.vandenbroeck@hotmail.com>
2021-05-01 18:22:29 +0200

    current winners page, css and needed imgs + first subpage

---
+++ client/src/pages/winners/winter.js
@@ -0,0 +1,17 @@
+import './index.css';
+import winter from '../../imgs/winter-contest.png';
+
+function Winter() {
+  return (
+    <section className="">
+      <div class="info-intro">
+      <h1 class="trail-line-double">Winter Wonderland</h1>
+      <p>
+
+      </p>
+    </div>
+    </section>
+  );
+}
+
+export default Winter;
