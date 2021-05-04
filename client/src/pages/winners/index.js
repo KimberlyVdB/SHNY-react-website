456fdce263fd77f8e7cf10a518a994ddd283a70d
SugarcubeIsCoding <Kimberly.vandenbroeck@hotmail.com>
2021-05-04 14:25:00 +0200

    index finished, subpages with stories added

--- client/src/pages/winners/index.js
+++ client/src/pages/winners/index.js
@@ -1,67 +1,65 @@
 import './index.css';
-import winter from '../../imgs/winter-contest.png';
+import wintersday from '../../imgs/winter-contest.png';
 import halloween from '../../imgs/halloween-contest.png';
 import sab from '../../imgs/SAB-contest.png';
-
+import { Link } from "@reach/router"
 
 function competition() {
   return (
     <section className="contest-winners">
-    <div class="info-intro">
-      <h1 class="trail-line-double">Contest Winners</h1>
-      <p class="intro-winners">We hold screenshot contests on our discord server, they are always around a theme, sometimes its about the outfit and sometimes about the scene. Below you can find some of our winning skritt! Feast your eyes!
+      <div class="info-intro">
+        <h1 class="trail-line-double">Contest Winners</h1>
+        <p class="intro-winners">
+          We hold screenshot contests on our discord server,
+          they are always around a theme, sometimes its about the outfit
+          and sometimes about the scene. Below you can find some of our winning skritt! Feast your eyes!
+        </p>
+      </div>
 
-</p>
-    </div>
 
-
-    <div class="info-blocks ">
+      <div class="info-blocks ">
         <img class="info-img " alt="winter-wonderland" src={sab}></img>
+
         <div class="info-block-intro ">
           <h3 class="trail-line ">Super Adventure Box</h3>
           <ul class= "winners-ul">
             <li ><span class="winners-span">Winner:  </span> Luna</li>
             <li ><span class="winners-span">Won:</span> A blacklion skin weapon of choice worth 1 ticket</li>
           </ul>
-
         </div>
       </div>
 
       <div class="info-blocks">
-      <div class="info-block-intro">
-        <h3 class="trail-line">Winter Wonderland</h3>
-        <ul class= "winners-ul">
-            <li ><span class="winners-span">Winner:  </span> Rito</li>
-            <li ><span class="winners-span">Won:</span> A blacklion skin weapon of choice worth 1 ticket</li>
-          </ul>
-
-          {/* <li className="header-m__menu-item">
-                <Link to="/about">About</Link>
-              </li> */}
-
-          <div class="read-more">
-                <Link to="/winter" class="read-btn"> Read Story</Link>
-              </div>
-        </div>
-          <img class="info-img " alt="winter-wonderland" src={winter}></img>
-      </div>
-
-
-
-      <div class="info-blocks ">
-        <img class="info-img " alt="halloween" src={halloween}></img>
-        <div class="info-block-intro ">
-          <h3 class="trail-line ">Spooky Season</h3>
+        <div class="info-block-intro">
+          <h3 class="trail-line">Winter Wonderland</h3>
           <ul class= "winners-ul">
             <li ><span class="winners-span">Winner:  </span> Rito</li>
             <li ><span class="winners-span">Won:</span> A blacklion skin weapon of choice worth 1 ticket</li>
           </ul>
+
           <div class="read-more">
-                <a href="/" className="read-btn">Read Story</a>
-              </div>
+            <Link to="/winter" className="read-btn"> Read Story</Link>
+          </div>
         </div>
+
+        <img class="info-img " alt="winter-wonderland" src={wintersday}></img>
       </div>
 
+      <div class="info-blocks ">
+        <img class="info-img " alt="halloween" src={halloween}></img>
+
+        <div class="info-block-intro ">
+          <h3 class="trail-line ">Spooky Season</h3>
+          <ul class= "winners-ul">
+            <li ><span class="winners-span">Winner: </span> Rito</li>
+            <li ><span class="winners-span">Won:</span> A blacklion skin weapon of choice worth 1 ticket</li>
+          </ul>
+
+          <div class="read-more">
+            <Link to="/halloween" className="read-btn"> Read Story</Link>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- client/src/pages/winners/winter.js
+++ client/src/pages/winners/winter.js
@@ -1,15 +1,33 @@
 import './index.css';
-import winter from '../../imgs/winter-contest.png';
+import wintersday from '../../imgs/winter-contest.png';
 
 function Winter() {
   return (
     <section className="">
+      <div class="contain-div">
       <div class="info-intro">
       <h1 class="trail-line-double">Winter Wonderland</h1>
-      <p>
-
+      <p class="text-white">author: Rito</p>
+      </div>
+      <div class="story-div">
+      <p class="story-para">
+      Tired she was so tired. Only wanted to sleep and forget everything which had happened. Her parents death, her uncle’s death and.. her sisters...
       </p>
-    </div>
+      <p class="story-para">
+      Ljot begun dreaming. She was back, back at their homestead, where they had lived many years ago. Her parents had not yet died, and she was still only a child.
+      </p>
+      <p class="story-para">
+      It was that the perfect weather for a snowball fight. All the children wanted to play but Kára wasn’t allowed as she was only four years old. Therefore she stood at the side, crying, while her older sisters threw snowballs at each other.
+      </p>
+      <p class="story-para">
+      Ljot smiled slightly in her sleep while remembering. The snowball fight ended very abruptly as Eyia accidentally hit a son of Svanir. Who chased them around. The four Struludóttirs ran away together and hid behind the homestead, trying to keep themselves from bursting out in laughter as the Svanir looked everywhere for them.
+      </p>
+      <p class="story-para">
+      She truly loved her sisters, she couldn’t give up now, not when she had already came this far. Ljot had to protect them, and their smiling faces, trying not to laugh. Maybe they could all have a snowball fight again when this is all over, maybe...
+      </p>
+      </div>
+      <img class=" " alt="winter-wonderland" src={wintersday}></img>
+      </div>
     </section>
   );
 }
