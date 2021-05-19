4657adb7dc3ae2011020735ae09cd55593114b5f
Muhammed Bacalan <muhammedbacalan@gmail.com>
2021-05-19 14:12:42 +0000

    Update Strapi version and configs

--- server/config/server.js
+++ server/config/server.js
@@ -1,7 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://shny.mbacalan.tk/api/',
+  proxy: true,
   admin: {
+    url: '/admin/',
     auth: {
       secret: env('ADMIN_JWT_SECRET', '8f2844d139b7fea4d6158fc078d2d09d'),
     },
