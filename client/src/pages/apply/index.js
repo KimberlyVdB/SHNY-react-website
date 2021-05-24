5a25bc85706e035e4df1ad54d6aaecfa85954d6a
SugarcubeIsCoding <Kimberly.vandenbroeck@hotmail.com>
2021-05-24 20:18:42 +0200

    modal and other changes to apply

--- client/src/pages/apply/index.js
+++ client/src/pages/apply/index.js
@@ -1,22 +1,40 @@
 import './index.css';
+import { useState } from 'react';
 import applyImg from '../../imgs/skrittatheart1.png';
+import Modal from '../../components/modal';
 
-function apply() {
+
+function Apply() {
+  const [showModal, setShowModal] = useState(false);
+
+  function toggleModal() {
+    setShowModal(!showModal);
+  };
+
   return (
     <section className="apply">
       <div className="apply-intro">
         <h1 className="trail-line-double">Apply</h1>
-        <p>
+        <p className="apply-intro__text">
           So you want to apply? Not so fast! There are a few things you will need to learn and agree with before doing so!
           There arent many rules, the main goal is to have fun together as a guild.
           However, general etiquette, decency and maturity is expected. Some things we're strict about are;
         </p>
 
-        <p>
-          Racial, religious,sexual and/or any other type of discrimination will not be tolerated, even as jokes.
-          Same goes for homophobia and transphobia. Don't engage in religious/political or sexual discussions.
-          There is enough of that IRL. Don't be an ass
-        </p>
+        <ul className="apply-intro__list">
+          <li>
+            Racial, religious,sexual and/or any other type of discrimination will not be tolerated, even as jokes.
+            Same goes for homophobia and transphobia.
+          </li>
+
+          <li>
+            Don't engage in religious/political or sexual discussions. There is enough of that IRL.
+          </li>
+
+          <li>
+            Don't be an ass.
+          </li>
+        </ul>
       </div>
 
       <div className="rules-div">
@@ -65,48 +83,56 @@ function apply() {
       <div className="info-blocks info-blocks__apply">
         <img className="info-img" alt="apply" src={applyImg}></img>
         <div className="apply-form">
-          <h3 className="trail-line">Sounds good?</h3>
-          <form name="application" netlify>
-            <ul>
-              <li>
-                <input type="checkbox" id="rules" required></input>
-                <label htmlFor="rules" className="checkbox-label">I understand and accept the rules.</label>
-              </li>
+          <h3 className="trail-line">Sound good?</h3>
 
-              <li>
-                <input type="checkbox" id="eu" required></input>
-                <label htmlFor="eu" className="checkbox-label">I play on European servers.</label>
-              </li>
 
-              <li>
-                <input type="checkbox" id="discord" required></input>
-                <label htmlFor="discord" className="checkbox-label">I will join and use Discord.</label>
-              </li>
+          <Modal title="Application" active={showModal} close={() => toggleModal()}>
+            <form name="application" netlify="true">
+              <ul>
+                <li>
+                  <input type="checkbox" id="rules" required></input>
+                  <label htmlFor="rules" className="checkbox-label">I understand and accept the rules.</label>
+                </li>
 
-              <li>
-                <input type="checkbox" id="timeout" required></input>
-                <label htmlFor="timeout" className="checkbox-label">I understand I will be kicked if I'm offline for over 3 months without a notice.</label>
-              </li>
+                <li>
+                  <input type="checkbox" id="eu" required></input>
+                  <label htmlFor="eu" className="checkbox-label">I play on European servers.</label>
+                </li>
 
-              <li>
-                <label htmlFor="accname" className="label-input" required>Your ingame name:</label>
-                <input type="text" id="accname" name="accname" className="accname" placeholder="Notaskritt.1234"></input>
-              </li>
+                <li>
+                  <input type="checkbox" id="discord" required></input>
+                  <label htmlFor="discord" className="checkbox-label">I will join and use Discord.</label>
+                </li>
 
-              <li>
-                <label htmlFor="userMessage" className="label-input">Why would you like to join/ tell us about yourself below:</label>
-                <textarea id="userMessage" name="userMessage" className="usermessage" ></textarea>
-              </li>
-            </ul>
+                <li>
+                  <input type="checkbox" id="timeout" required></input>
+                  <label htmlFor="timeout" className="checkbox-label">I understand I will be kicked if I'm offline for over 3 months without a notice.</label>
+                </li>
 
-            <div className="apply-div">
-              <button type="submit" class="apply-btn">Apply Now</button>
-            </div>
-          </form>
+                <li>
+                  <label htmlFor="accname" className="label-input" required>Your ingame name:</label>
+                  <input type="text" id="accname" name="accname" className="accname" placeholder="Notaskritt.1234"></input>
+                </li>
+
+                <li>
+                  <label htmlFor="userMessage" className="label-input">Why would you like to join/ tell us about yourself below:</label>
+                  <textarea id="userMessage" name="userMessage" className="usermessage" ></textarea>
+                </li>
+              </ul>
+
+              <div className="apply-div">
+                <button type="submit" className="modal_btn">Submit</button>
+              </div>
+            </form>
+          </Modal>
+
+          <div className="apply-div">
+            <button onClick={toggleModal} className="apply-btn">Apply Now</button>
+          </div>
         </div>
       </div>
     </section>
   );
 }
 
-export default apply;
+export default Apply;
