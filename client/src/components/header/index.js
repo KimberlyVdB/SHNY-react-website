37eb70665a91f1b89a2f2b3e4b710b548848f1fa
Muhammed Bacalan <muhammedbacalan@gmail.com>
2022-03-07 19:34:08 +0300

    (client) Fix header glitching on certain screens

--- client/src/components/header/index.js
+++ client/src/components/header/index.js
@@ -11,7 +11,6 @@ class Header extends Component {
     this.listener = null;
     this.state = {
       mobileActive: false,
-      nav: false,
       menuItems: [
         { name: "Home", path: "/" },
         { name: "About", path: "/about" },
@@ -26,22 +25,6 @@ class Header extends Component {
 
   };
 
-  componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-  }
-
-  handleScroll = () => {
-    if (window.pageYOffset > 200) {
-      if (!this.state.nav) {
-        this.setState({ nav: true });
-      }
-    } else {
-      if (this.state.nav) {
-        this.setState({ nav: false });
-      }
-    }
-  }
-
   handleClick(e) {
     e.preventDefault();
     this.setState(state => ({ mobileActive: !state.mobileActive }));
@@ -49,61 +32,58 @@ class Header extends Component {
 
   render() {
     let mobileNavClass = 'header-m__content';
-    let scrollNavClass = '';
 
     if (this.state.mobileActive) {
       mobileNavClass += ' active';
     }
 
-    if (this.state.nav) {
-      scrollNavClass = 'header-d__scroll';
-    }
-
     return (
       <header className="header">
-        <div className={'header-d ' + scrollNavClass} >
-          <NavLink to="/">
-            <img className="header-d__logo" src={logo} alt="SHNY-logo"></img>
-          </NavLink>
+        {window.innerWidth > 540 ? (
+          <>
+            <NavLink to="/">
+              <img className="header-d__logo" src={logo} alt="SHNY-logo"></img>
+            </NavLink>
 
-          <ul className="header-d__menu">
-            {this.state.menuItems.map(item =>
-              <li className="header-d__menu-item" key={item.name}>
-                <NavLink to={item.path} key={item.name}>
-                  {item.name}
-                </NavLink>
-              </li>
-            )}
-          </ul>
-        </div>
-
-        <div className="header-m">
-          <div className="header-m__banner">
-            <img className="header-m__logo" src={logo} alt="SHNY-logo"></img>
-            <a href="#!" className="header-m__open" onClick={this.handleClick}>
-              <img src={hamburger} alt="hamburger-icon"></img>
-            </a>
-          </div>
-
-          <div className={mobileNavClass + ' ' + scrollNavClass}>
-            <div className="header-m-top">
-              <img className="header-m__logo" src={logo} alt="SHNY-logo"></img>
-              <a href="#!" onClick={this.handleClick}>
-                <img className="close-nav" src={close} alt="close icon"></img>
-              </a>
-            </div>
-
-            <ul className="header-m__menu">
+            <ul className="header-d__menu">
               {this.state.menuItems.map(item =>
-                <li className="header-m__menu-item" onClick={this.handleClick} key={item.name}>
+                <li className="header-d__menu-item" key={item.name}>
                   <NavLink to={item.path} key={item.name}>
                     {item.name}
                   </NavLink>
                 </li>
               )}
             </ul>
-          </div>
-        </div>
+          </>
+        ) : (
+          <>
+            <div className="header-m__banner">
+              <img className="header-m__logo" src={logo} alt="SHNY-logo"></img>
+              <a href="#!" className="header-m__open" onClick={this.handleClick}>
+                <img src={hamburger} alt="hamburger-icon"></img>
+              </a>
+            </div>
+
+            <div className={mobileNavClass}>
+              <div className="header-m-top">
+                <img className="header-m__logo" src={logo} alt="SHNY-logo"></img>
+                <a href="#!" onClick={this.handleClick}>
+                  <img className="close-nav" src={close} alt="close icon"></img>
+                </a>
+              </div>
+
+              <ul className="header-m__menu">
+                {this.state.menuItems.map(item =>
+                  <li className="header-m__menu-item" onClick={this.handleClick} key={item.name}>
+                    <NavLink to={item.path} key={item.name}>
+                      {item.name}
+                    </NavLink>
+                  </li>
+                )}
+              </ul>
+            </div>
+          </>
+        )}
       </header >
     );
   };
